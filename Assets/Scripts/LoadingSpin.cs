e8b66c5e4f3e2397aac4c72a738c60abe3b980c9
Jon Fus <jonfus123@hotmail.com>
2019-12-03 01:20:48 -0500

    Update Lobby Scene Added LoadingSpin script to rotate a ui spinner

---
+++ Assets/Scripts/LoadingSpin.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingSpin : MonoBehaviour
+{
+
+    public Image spinner;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spinner == null)
+            print("LoadingSpin: Loading Spinner Image not found.");
+        else
+            spinner.transform.Rotate(0.0f, 0.0f, 1.0f);
+    }
+}
