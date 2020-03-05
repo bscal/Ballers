3eb87e06e82ae27610c5d06b596337c67c8dbe97
Jon Fus <jonfus123@hotmail.com>
2020-03-04 21:56:02 -0500

    Small update to test and work with backend server

--- Assets/Scripts/Networking/NetworkClientToBackend.cs
+++ Assets/Scripts/Networking/NetworkClientToBackend.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Steamworks;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -7,13 +8,21 @@ public class NetworkClientToBackend : MonoBehaviour
 {
     void Start()
     {
-        print("trying to log in");
-        StartCoroutine(Login());
+        print("trying to log in...");
+        StartCoroutine(Login(SteamUser.GetSteamID().m_SteamID));
+
+        if (SteamManager.Initialized)
+        {
+            string name = SteamFriends.GetPersonaName();
+            print(name);
+
+            print(SteamUser.GetSteamID());
+        }
     }
 
-    public IEnumerator Login()
+    public IEnumerator Login(ulong steamid)
     {
-        using (UnityWebRequest webRequest = UnityWebRequest.Get("bscal.me:9090"))
+        using (UnityWebRequest webRequest = UnityWebRequest.Get("bscal.me:9090/login/" + steamid))
         {
             yield return webRequest.SendWebRequest();
 
