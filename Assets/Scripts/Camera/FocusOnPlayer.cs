ec6da6398dbea94654cf2a9baf52d6e6a98e48fa
Jon Fus <jonfus123@hotmail.com>
2020-04-18 22:29:54 -0400

    - MainMenu and TestModel prefab updates - More work on MainMenu scene and ui - Added MenuSetup to handle MainMenu scene - Updated FocusOnPlayer script to be more functional. - Updated TestModel prefab mostly picavoxel objects - Added Player model previewer to character view in MainMenu

--- Assets/Scripts/Camera/FocusOnPlayer.cs
+++ Assets/Scripts/Camera/FocusOnPlayer.cs
@@ -1,20 +1,37 @@
-﻿using MLAPI.Spawning;
+﻿using MLAPI;
+using MLAPI.Spawning;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FocusOnPlayer : MonoBehaviour
 {
+    public bool startDisabled = true;
     public Vector3 offset;
+    public float rotation;
 
-    void Awake()
+    private GameObject m_target;
+
+    void Start()
     {
-        gameObject.SetActive(false);
+        if (NetworkingManager.Singleton == null)
+        {
+            m_target = GameObject.Find("Player");
+        }
+        else
+        {
+            m_target = SpawnManager.GetLocalPlayerObject().gameObject;
+        }
+        if (startDisabled)
+            gameObject.SetActive(false);
+        transform.Rotate(rotation, 0, 0);
     }
 
     void Update()
     {
-        transform.position = SpawnManager.GetLocalPlayerObject().gameObject.transform.position + offset;
-        print("updated");
+        if (m_target == null)
+            m_target = SpawnManager.GetLocalPlayerObject().gameObject;
+
+        transform.position = m_target.transform.position + offset;
     }
 }
---
+++ Assets/Scripts/Menu/MenuSetup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSetup : MonoBehaviour
+{
+
+    public GameObject modelPrefab;
+    private GameObject m_model;
+
+    void Awake()
+    {
+        if (modelPrefab)
+        {
+            m_model = Instantiate(modelPrefab, Vector3.zero, Quaternion.identity);
+            m_model.name = "Player";
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+}
