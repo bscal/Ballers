b0311a2f445e19e25d09032cb557798daf7eef8d
Jon Fus <jonfus123@hotmail.com>
2020-04-20 23:45:53 -0400

    Small Update testing SteamMatchmaking lobbies

--- Assets/Scripts/Matchmaking/Matchmaking.cs
+++ Assets/Scripts/Matchmaking/Matchmaking.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections;
+﻿using Steamworks;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Matchmaking : MonoBehaviour
 {
     public bool IsFinding { get; set; } = false;
+
+    private CallResult<LobbyMatchList_t> m_CallResultLobbyMatchList = new CallResult<LobbyMatchList_t>();
 
     private float m_timer = 0f;
 
@@ -26,7 +29,15 @@ public class Matchmaking : MonoBehaviour
     {
         IsFinding = true;
 
+        SteamAPICall_t result = SteamMatchmaking.RequestLobbyList();
+        m_CallResultLobbyMatchList.Set(result, OnLobbyMatchList);
     }
+
+    private void OnLobbyMatchList(LobbyMatchList_t lobbyMatchList, bool bIOfailure)
+    {
+        print(lobbyMatchList.m_nLobbiesMatching);
+    }
+
 
     public void StopFinding()
     {
