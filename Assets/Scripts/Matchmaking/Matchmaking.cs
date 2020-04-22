635b0067bce2bf8f41126a2283e1f100f67fbf50
Jon Fus <jonfus123@hotmail.com>
2020-04-22 00:32:14 -0400

    Added more steam matchmaking and lobby stuff

--- Assets/Scripts/Matchmaking/Matchmaking.cs
+++ Assets/Scripts/Matchmaking/Matchmaking.cs
@@ -7,7 +7,8 @@ public class Matchmaking : MonoBehaviour
 {
     public bool IsFinding { get; set; } = false;
 
-    private CallResult<LobbyMatchList_t> m_CallResultLobbyMatchList = new CallResult<LobbyMatchList_t>();
+    private readonly CallResult<LobbyMatchList_t> m_CallResultLobbyMatchList = new CallResult<LobbyMatchList_t>();
+    private readonly CallResult<LobbyCreated_t> m_CallResultLobbyCreated = new CallResult<LobbyCreated_t>();
 
     private float m_timer = 0f;
 
@@ -29,15 +30,51 @@ public class Matchmaking : MonoBehaviour
     {
         IsFinding = true;
 
+        // This filters were lobbies with correct gamemodes this is required before RequestLobbyList
+        SteamMatchmaking.AddRequestLobbyListStringFilter("Gamemode", "Ballers-1v1", ELobbyComparison.k_ELobbyComparisonEqual);
         SteamAPICall_t result = SteamMatchmaking.RequestLobbyList();
         m_CallResultLobbyMatchList.Set(result, OnLobbyMatchList);
     }
 
     private void OnLobbyMatchList(LobbyMatchList_t lobbyMatchList, bool bIOfailure)
     {
-        print(lobbyMatchList.m_nLobbiesMatching);
+        uint num = lobbyMatchList.m_nLobbiesMatching;
+        print(num);
+
+        // If no lobbies create one
+        if (num < 1)
+        {
+            SteamAPICall_t result = SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypePublic, 2);
+            m_CallResultLobbyCreated.Set(result, OnLobbyCreated);
+        }
     }
 
+    private void OnLobbyCreated(LobbyCreated_t lobbyCreated, bool bIOfailure)
+    {
+        // Sets lobby gamemode
+        SteamMatchmaking.SetLobbyData(new CSteamID(lobbyCreated.m_ulSteamIDLobby), "Gamemode", "Ballers-1v1");
+        print("created lobby waiting to test 3secs...");
+        StartCoroutine(Test(lobbyCreated.m_ulSteamIDLobby));
+    }
+
+    // FOR TESTING
+    private IEnumerator Test(ulong l)
+    {
+        //test if lobby is created and leaves lobby (destroys on steams end)
+        yield return new WaitForSeconds(3.0f);
+        SteamMatchmaking.AddRequestLobbyListStringFilter("Gamemode", "Ballers-1v1", ELobbyComparison.k_ELobbyComparisonEqual);
+        SteamAPICall_t result = SteamMatchmaking.RequestLobbyList();
+        m_CallResultLobbyMatchList.Set(result, OnLobbyMatchListTest);
+        yield return new WaitForSeconds(1.0f);
+        SteamMatchmaking.LeaveLobby(new CSteamID(l));
+    }
+
+    // FOR TESTING
+    private void OnLobbyMatchListTest(LobbyMatchList_t lobbyMatchList, bool bIOfailure)
+    {
+        uint num = lobbyMatchList.m_nLobbiesMatching;
+        print(num);
+    }
 
     public void StopFinding()
     {
