741713d7c45e27a6b80d37e688aef85e69172d38
Jon Fus <jonfus123@hotmail.com>
2020-04-24 22:20:25 -0400

    Updated to matchmaking and match setup

---
+++ Assets/Scripts/Matchmaking/MatchSetup.cs
@@ -0,0 +1,50 @@
+﻿using Steamworks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameType
+{
+    ONES,
+    THREES,
+    FIVES
+}
+
+public class MatchSetup : MonoBehaviour
+{
+
+    private const string GAME_SCENE_NAME = "";
+
+    public bool HasStarted { get; private set; }
+    public ulong MatchID { get; private set; }
+    public CSteamID HostID { get; private set; }
+
+    private Dictionary<byte, ulong> m_playerSlots = new Dictionary<byte, ulong>();
+    private GameType m_type;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void LoadGameScene()
+    {
+        HasStarted = true;
+    }
+
+    private IEnumerator ConnectToServer()
+    {
+        yield return null;
+    }
+
+    private void AfterLoadGameScene()
+    {
+
+    }
+
+}
