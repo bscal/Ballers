de867a011bc29cad7e1107b7fc8522b349e09aad
Jon Fus <jonfus123@hotmail.com>
2020-05-26 18:45:18 -0400

    Added MatchGlobals script. A static class for helping with match parameters and variables

---
+++ Assets/Scripts/Game/MatchGlobals.cs
@@ -0,0 +1,33 @@
+﻿using Steamworks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class MatchGlobals
+{
+    public static NetworkLobby NetworkLobby { get; set; }
+    public static BallersGamemode GameMode { get; set; }
+    public static int TeamSize { get; set; }
+    public static int QuarterLength { get; set; }
+    public static int QuartersCount { get; set; }
+    public static ulong MatchID { get; set; }
+    public static CSteamID HostID { get; set; }
+
+    public static bool HasGameStarted { get { return GameManager.Singleton.HasStarted; } }
+    public static bool HasLoadedGame { get; set; }
+    public static bool HasJoinedLobby { get; set; }
+    public static bool HostServer { get; set; }
+
+    public static void ResetDefault()
+    {
+        NetworkLobby = null;
+        MatchID = 0;
+        HostID = CSteamID.Nil;
+        HasLoadedGame = false;
+        HasJoinedLobby = false;
+        HostServer = false;
+    }
+
+}
