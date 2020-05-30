c4b32a9aae863b0430a618e7f4f69066f85ddb2d
Jon Fus <jonfus123@hotmail.com>
2020-05-30 17:00:58 -0400

    Updated ServerState to handle player loading

--- Assets/Scripts/Server/ServerState.cs
+++ Assets/Scripts/Server/ServerState.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 public static class ServerState
 {
+
     public static Dictionary<ulong, ServerPlayer> Players = new Dictionary<ulong, ServerPlayer>();
 
     public static void HandlePlayerConnection(ulong steamId)
@@ -17,7 +20,60 @@ public static class ServerState
         }
         else
         {
-            ServerState.Players.Add(steamId, new ServerPlayer(steamId));
+            Players.Add(steamId, new ServerPlayer(steamId));
         }
+    }
+
+    public static IEnumerator PlayersLoadedCoroutine(float timeout)
+    {
+        const float WAIT_TIME = 3f;
+        float timer = 0f;
+        while (timeout > timer)
+        {
+            timer += WAIT_TIME;
+            if (HaveAllPlayersLoaded())
+            {
+                StartMatch();
+                break;
+            }
+            if (timer > timeout)
+            {
+                DestroyMatch();
+                break;
+            }
+            yield return new WaitForSeconds(WAIT_TIME);
+        }
+    }
+
+    private static bool HaveAllPlayersLoaded()
+    {
+        if (Players.Count < MatchGlobals.NeededPlayers)
+            return false;
+        foreach (ServerPlayer sp in Players.Values)
+        {
+            if (sp.status == ServerPlayerStatus.DISCONNECTED)
+            {
+                return false;
+            }
+        }
+        // All players have loaded
+        return true;
+
+
+    }
+
+    private static void StartMatch()
+    {
+        Debug.Log("starting match");
+    }
+
+    private static void DestroyMatch()
+    {
+        Debug.Log("destroying match");
+    }
+
+    public static void MigrateHost()
+    {
+
     }
 }
