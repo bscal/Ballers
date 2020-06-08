6785f976109a0c58eda5a741cc8c72dfa3513c23
Jon Fus <jonfus123@hotmail.com>
2020-06-07 20:32:15 -0400

    Added a BuildScript that will handle building builds for a script im creating

---
+++ Assets/Editor/Build/BuildScript.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+public static class BuildScript
+{
+    static readonly string[] scenes = { 
+        "Assets/scenes/MainMenu.unity",
+        "Assets/scenes/Match.unity",
+    };
+
+    public static void Win64()
+    {
+        string pathToDeploy = "builds/Windows64/Ballers.exe";
+
+        var report = BuildPipeline.BuildPlayer(scenes, pathToDeploy, BuildTarget.StandaloneWindows64, BuildOptions.None);
+
+        Debug.Log(report.summary);
+    }
+
+    public static void Linux64()
+    {
+        string pathToDeploy = "builds/Linux64/Ballers.x86_64";
+
+        var report = BuildPipeline.BuildPlayer(scenes, pathToDeploy, BuildTarget.StandaloneLinux64, BuildOptions.None);
+
+        Debug.Log(report.summary);
+    }
+
+}
