11d8e97688dd67d97ab2451d2327d610304c24b2
Jon Fus <jonfus123@hotmail.com>
2020-06-10 23:34:59 -0400

    Added UIExitButtonListener and exit button to Match scene

---
+++ Assets/Scripts/UI/ButtonListeners/UIExitButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIExitButton : MonoBehaviour
+{
+
+    public void OnExitButtonPressed()
+    {
+        Application.Quit();
+    }
+
+}
