6dc7fe6e504403f14bffac617fb752c93900dc4e
Jon Fus <jonfus123@hotmail.com>
2020-08-13 23:45:12 -0400

    Added Enumeration util script to have class like enums
    - Added AIDifficulty Enumeration class
    - Added BallerPosition Enumeration class

---
+++ Assets/Scripts/Util/Enums/AIDifficulty.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIDifficulty : Enumeration
+{
+    public readonly static AIDifficulty ROOKIE = new AIDifficulty(0, "Rookie", .75f);
+    public readonly static AIDifficulty PRO = new AIDifficulty(0, "Pro", 1.0f);
+    public readonly static AIDifficulty ALL_STAR = new AIDifficulty(0, "All Star", 1.25f);
+
+    public float Modifier { get; private set; }
+
+    AIDifficulty(int id, string name, float modifier)
+        : base(id, name)
+    {
+        Modifier = modifier;
+    }
+
+}
---
+++ Assets/Scripts/Util/Enums/BallerPosition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallerPosition : Enumeration
+{
+    public readonly static BallerPosition PG = new BallerPosition(0, "PG");
+    public readonly static BallerPosition SG = new BallerPosition(0, "SG");
+    public readonly static BallerPosition SF = new BallerPosition(0, "SF");
+    public readonly static BallerPosition PF = new BallerPosition(0, "PF");
+    public readonly static BallerPosition C = new BallerPosition(0, "C");
+
+    public BallerPosition(int id, string name)
+        : base(id, name)
+    {
+
+    }
+
+}
