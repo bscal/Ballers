877d24f374508b9f90f2d5843e9675ec23653152
Jon Fus <jonfus123@hotmail.com>
2020-10-06 21:03:19 -0400

    Updated animations. Added LeftBall and RightBall objects to Armature for ball

---
+++ Assets/Scripts/ModelDemoScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelDemoScript : MonoBehaviour
+{
+
+    public GameObject leftHandPos;
+    public GameObject rightHandPos;
+    public GameObject ballObject;
+
+    public Animator animator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ballObject.transform.position = rightHandPos.transform.position;
+    }
+}
